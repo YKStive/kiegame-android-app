1e08ffa0b9d6c2e3e09bedae798a92c09870c32d
var_rain <var_rain@outlook.com>
2020-04-02 23:45:02 +0800

    修改加密模式

--- app/src/main/java/com/kiegame/mobile/utils/AES.java
+++ app/src/main/java/com/kiegame/mobile/utils/AES.java
@@ -1,11 +1,12 @@
 package com.kiegame.mobile.utils;
 
+import android.annotation.SuppressLint;
+
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -23,11 +24,12 @@ public class AES {
      * @param data 待加密内容
      * @return 返回Base64转码后的加密数据
      */
+    @SuppressLint("GetInstance")
     public static String encrypt(String data) {
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-            cipher.init(Cipher.ENCRYPT_MODE, AES.key(), new IvParameterSpec(new byte[cipher.getBlockSize()]));
+            cipher.init(Cipher.ENCRYPT_MODE, AES.key());
             byte[] result = cipher.doFinal(bytes);
             return BASE64.encrypt(result);
         } catch (Exception e) {
@@ -42,10 +44,11 @@ public class AES {
      * @param data 待解密内容
      * @return 返回解密后的数据
      */
+    @SuppressLint("GetInstance")
     public static String decrypt(String data) {
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, AES.key(), new IvParameterSpec(new byte[cipher.getBlockSize()]));
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, AES.key());
             byte[] result = cipher.doFinal(BASE64.decrypt(data));
             return new String(result, StandardCharsets.UTF_8);
         } catch (Exception e) {
